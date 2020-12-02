387b62d38defdfcc8523bfcc350b986b9d4e2c48
Teddir <ferryaja20000@gmail.com>
2020-12-02 10:03:44 +0700

    Chat

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminTableSeeder::class);
+        // $this->call(AdminTableSeeder::class);
         $this->call(PenjualTableSeeder::class);
         $this->call(ProdukTableSeeder::class);
 
